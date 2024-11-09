4f2e42c5c08b5b470f5c232f1986dd7443a3fe36
Bl0tniaQus <focusedmud@wp.pl>
2024-11-09 11:41:22 +0100

    readme

---
+++ Readme.md
@@ -0,0 +1,2 @@
+```
+g++ main.cpp -I "./ga/include" -Wall -o "main"
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "ga/algorithm.hpp"
+
+int main()
+{
+        std::cout<<"knapsack_problem_ga\n";
+
+return 0;    
+}
